c3017e8afbe4ec9259eef9a8cc1ef2cdaeb0c95d
JangHongJoon <37648375+JangHongJoon@users.noreply.github.com>
2021-02-15 17:38:11 +0900

    2021.02.15

---
+++ chap03/src/operation06/exam01/InputDataCheckNaNExample1.java
@@ -0,0 +1,15 @@
+package operation06.exam01;
+
+public class InputDataCheckNaNExample1 {
+	public static void main(String[] args) {
+		String userInput = "NaN";
+		double val = Double.valueOf( userInput );
+		
+		double currentBalance = 10000.0;
+		
+		currentBalance += val;
+		
+		System.out.println(currentBalance);
+		
+	}
+}
